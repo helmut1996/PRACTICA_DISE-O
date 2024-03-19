296a215d9efe5736e145ea9061190dc1a7ab6e2a
helmut1996 <josubrenes0@gmail.com>
2024-03-19 11:45:36 -0600

    Creacion de DialogWarning

--- app/src/main/java/com/example/practicacompose/MainActivity.kt
+++ app/src/main/java/com/example/practicacompose/MainActivity.kt
@@ -3,24 +3,34 @@ package com.example.practicacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.example.practicacompose.components.DialogWarning
 import com.example.practicacompose.ui.theme.PracticaComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             PracticaComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ReportMeterView()
+                    content()
                 }
             }
         }
@@ -28,9 +38,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun content(){
+fun content() {
+    var showDialog by remember { mutableStateOf(false) }
 
+    Column {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Dialog warning")
+        }
 
+        if (showDialog) {
+            DialogWarning(
+                onDismissRequest = { showDialog = false },
+                onConfirmationRequest = { showDialog = false }
+            )
+        }
+    }
 
 }
 
---
+++ app/src/main/java/com/example/practicacompose/components/DialogWarning.kt
@@ -0,0 +1,75 @@
+package com.example.practicacompose.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.practicacompose.R
+
+@Composable
+fun DialogWarning(onDismissRequest: () -> Unit, onConfirmationRequest: () -> Unit) {
+    Dialog(onDismissRequest = { /*TODO*/ }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(255.dp)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_warning),
+                    contentDescription = "",
+                    modifier = Modifier.size(100.dp),
+                    tint = Color(0xFFF57F17)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Este es un dialog de advertencia")
+                Spacer(modifier = Modifier.height(30.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth() .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Button(
+                        onClick = { onDismissRequest() }, colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Red
+                        )
+                    ) {
+                        Text(text = "CANCELAR")
+                    }
+
+
+                    Button(
+                        onClick = { onDismissRequest() }, colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Blue,
+
+                        ),
+
+                    ) {
+                        Text(text = "ACEPTAR")
+                    }
+
+                }
+            }
+        }
+    }
+}
